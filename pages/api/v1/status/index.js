62891a116705ab556516f3982620a6fe9b2d818e
Carlos Eduardo de Souza <52358615+sou-carlos@users.noreply.github.com>
2024-08-19 03:07:36 +0000

    add `database.js` and `.env`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,6 +1,12 @@
 //next automaticamente cria rota publica /api/v1/status
 //next injeta objeto de request e response
-function status(request, response) {
+
+import database from "/infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 as soma;");
+  console.log(result.rows);
+
   response.status(200).json({
     mensagem: "Tudo certo com o endpoint, bão demais",
   });
