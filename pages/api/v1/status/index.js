1e6e1c59632d900dfdfdb04238c406af1d810c12
Carlos Eduardo de Souza <52358615+sou-carlos@users.noreply.github.com>
2024-08-23 00:17:36 +0000

    criando novo response do /status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,14 +1,11 @@
 //next automaticamente cria rota publica /api/v1/status
 //next injeta objeto de request e response
 
-import database from "infra/database.js";
-
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as soma;");
-  console.log(result.rows);
+  const updatedAt = new Date().toISOString();
 
   response.status(200).json({
-    mensagem: "Tudo certo com o endpoint, bão demais",
+    updated_at: updatedAt,
   });
 }
 
